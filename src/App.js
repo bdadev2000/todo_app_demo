259dc84fbf796129a093eb8ff1e728d732d58994
Duc Anh <bdadevfs@gmail.com>
2021-10-20 00:44:40 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container,CssBaseline,Typography,TextField,Button, Grid } from '@material-ui/core';
+import React, {  useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+const App = ()=>{
+  
+  const [job,setJob] = useState();
+  const [jobs,setJobs] = useState(()=>{
+    const storage = JSON.parse(localStorage.getItem('jobs'))
+    return storage ?? []
+  });
+  
+  const handleSubmit = () =>{
+    setJobs(prev => {
+      const newJob = [...prev,job];
+      const jsonJobs = JSON.stringify(newJob)
+      localStorage.setItem('jobs',jsonJobs);
+      return newJob;
+    });
+    setJob('')
+  }
+
+  const handleDelete = (i) =>{
+    setJobs((jobs) => {
+      const jobList  = jobs.filter((job) => job !== jobs[i]);
+      localStorage.setItem('jobs',JSON.stringify(jobList));
+      return jobList;
+    })
+  }
+  
+   return(
+
+      <Container component="main" maxWidth="xs" >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 100,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent:'center'
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          
+          <Typography component="h1" variant="h5">
+            To Do List
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Add to do"
+              value={job}
+              onChange={e => setJob(e.target.value)}
+            />
+            
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              color="secondary"
+             onClick={handleSubmit}
+            >
+              Add
+            </Button>
+          </Box>
+
+          <Box component="form" noValidate sx={{ mt: 4 }}>
+            <ul style={{listStyle:"none"}}>
+               {
+                 jobs.map((job,index) => (
+                   <Grid container direction="row" justifyContent="space-between">
+                     <li key={index}>{job}</li>
+                     <button style={{margin:4}} onClick={() => handleDelete(index)}> X </button>
+                   </Grid>
+                 ))
+               }
+            </ul>
+          </Box>
+        </Box>
+       
+      </Container>
+   )
 }
 
 export default App;
